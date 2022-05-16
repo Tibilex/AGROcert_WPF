a55bac2752cb576a44c9f1b1935cacb9d490ebc8
ibozic90 <ibozic90@gmail.com>
2022-05-16 17:07:12 +0300

    Add: pages Add: page"Home" Add: page"AboutUs" Add: page"Shop" Add: page"Pages" Add: page"ContactUs"

--- AGROcert/Windows/MainWindow.xaml.cs
+++ AGROcert/Windows/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AGROcert.ViewModels;
+using AGROcert.Windows.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,28 +23,70 @@ namespace AGROcert
     public partial class MainWindow : Window
     {
         private static MainWindow s_window;
+        private List<Page> _listPages;
 
         public MainWindow()
         {
             InitializeComponent();
-
             s_window = this;
+
+            _listPages = new List<Page>();
+            _listPages.Add(new HomePage());
+            _listPages.Add(new AboutUsPage());
+            _listPages.Add(new ShopPage());
+            _listPages.Add(new PagesPage());
+            _listPages.Add(new ContactUsPage());
+
+            progpamPages.Content = _listPages[0];
+
         }
 
         private void CloseWindow_Click(object sender, EventArgs e)
         {
             Application.Current.Shutdown();
         }
+
         private void MinimiseWindow_Click(object sender, EventArgs e)
         {
             this.WindowState = WindowState.Minimized;
         }
+
         private void WindowMoving_Click(object sender, MouseButtonEventArgs e)
         {
             if (Mouse.LeftButton == MouseButtonState.Pressed)
             {
                 s_window.DragMove();
             }
+        }
+
+        private void CartBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            progpamPages.Content = _listPages[0];
+        }
+
+        private void AboutBtn_Click(object sender, RoutedEventArgs e)
+        {
+            progpamPages.Content = _listPages[1];
+        }
+
+        private void MarketBtn_Click(object sender, RoutedEventArgs e)
+        {
+            progpamPages.Content = _listPages[2];
+        }
+
+        private void PagesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            progpamPages.Content = _listPages[3];
+        }
+
+        private void ContactsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            progpamPages.Content = _listPages[4];
         }
     }
 }
